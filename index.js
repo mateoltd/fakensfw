2c9c2565a9e1a42b1aa55857e5a8d4c762d54d31
Mathéo <96652538+soyzer0@users.noreply.github.com>
2022-03-08 22:14:01 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+var findModule=(item)=>window.webpackChunkdiscord_app.push([[Math.random()],{},(req)=>{for(const m of Object.keys(req.c).map((x)=>req.c[x].exports).filter((x)=>x)){if(m.default&&m.default[item]!==undefined)return m.default}}])
+findModule('getCurrentUser').getCurrentUser().nsfwAllowed = true
+
+// Créditos al respectivo autor, solo está aquí de modo informativo.
